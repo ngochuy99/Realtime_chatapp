33bad844ec0bccfaaf5546c88d72356e971a5f06
Nguyen Ngoc Huy <ngngochuy1107@gmail.com>
2020-08-11 09:47:48 +0700

    Add login + register api

--- Server/app.js
+++ Server/app.js
@@ -3,10 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+require('dotenv').config();
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var cors = require('cors'); 
+var UserApi = require('./routes/users');
+var db=require('./models/index.js');
 var app = express();
 
 // view engine setup
@@ -19,9 +20,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', UserApi);
+// app.user(Auth);
 
+db.sequelize.sync().then(async function(){
+    console.log('model sync process finished');
+})
+app.use(cors());
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -37,5 +42,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+app.listen(process.env.PORT,function(){
+  console.log('server is running on port '+ process.env.PORT);
+});
 module.exports = app;
--- Server/routes/users.js
+++ Server/routes/users.js
@@ -1,9 +1,62 @@
 var express = require('express');
 var router = express.Router();
+var hash=require('sha1');
+//Hash algorithm for password is sha1
+var db = require("../models/index");
+const { Op } = require("sequelize");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/login', async function(req, res) {
+  try {
+    var user_info=await Checklogin(req.body);
+    res.status(200).json({
+      Name:user_info.Name,
+      Email:user_info.Email
+    });
+  } 
+  catch (error) {
+    res.status(500).json({
+      error:"Wrong username or password"
+    });
+    throw error;
+  }
 });
 
+router.post('/register',async function(req,res){
+  try {
+    await Register(req.body);
+    res.status(200).json({
+      message:"success"
+    });
+  } 
+  catch (error) {
+    res.json({
+      message:error
+    });
+    throw error;
+  }
+})
+let Checklogin=async function(user_info){
+  var hashPass=await hashPassword(req.body.Password);
+    var user_info=await db.user.findAll({
+      attributes:['Name','Username','Password','Email'],
+      where:{
+        [Op.and]:[{Username:req.body.Username},{Password:hashPass}]
+      }
+    });
+    return user_info;
+}
+
+let Register =async function(user_info){
+  var hashPass=await hashPassword(user_info.Password);
+  await db.user.create({
+    Name:user_info.Name,
+    Username:user_info.Username,
+    Password:hashPass,
+    Email:user_info.Email
+  });
+}
+
+let hashPassword=async function(Password){
+    return hash(Password);
+}
 module.exports = router;
