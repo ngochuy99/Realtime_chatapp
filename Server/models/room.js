492e41fc537e36db82ab7857da627d816ea8fe1b
Nguyen Ngoc Huy <nh01272639408@gmail.com>
2020-09-10 11:21:04 +0700

    add Room model

---
+++ Server/models/room.js
@@ -0,0 +1,23 @@
+const {DataTypes} = require('sequelize');
+module.exports = function(sequelize,Sequelize){
+    var room = sequelize.define('room',{
+        ID:{
+            type:DataTypes.INTEGER,
+            autoIncrement:true,
+            primaryKey:true
+        },
+        name:{
+            type:DataTypes.STRING,
+            allowNull:false
+        },
+        password:{
+            type:DataTypes.STRING,
+            allowNull:true
+        }
+    }
+    );
+    room.associate  = (models)=>{
+        room.hasMany(models.user);
+    }
+    return room;
+}
--- Server/models/user.js
+++ Server/models/user.js
@@ -1,4 +1,5 @@
 const { DataTypes } = require("sequelize");
+const room = require('./room');
 module.exports=function(sequelize,Sequelize){
     var User=sequelize.define("user",{
         ID:{
@@ -22,6 +23,9 @@ module.exports=function(sequelize,Sequelize){
             type:DataTypes.STRING,
             allowNull:false
         }
-    })
+    });
+    User.associate  = (models)=>{
+        User.belongsTo(models.room); // A BelongsToMany B through the junction table C
+    }
     return User;
 }
