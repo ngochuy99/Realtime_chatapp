cdb80a0be280a6dd70776a60144eef8e3f56ca4a
Nguyen Ngoc Huy <nh01272639408@gmail.com>
2020-09-11 17:52:16 +0700

    Remove console log

--- Server/routes/room.js
+++ Server/routes/room.js
@@ -5,7 +5,6 @@ var db = require('../models/index');
 
 router.get('/listroom',async function(req,res){
     try {
-        console.log(req);
       var roomlist = await GetRoomList();  
       res.status(200).json({
           roomlist:roomlist
@@ -17,11 +16,37 @@ router.get('/listroom',async function(req,res){
         throw error;
     }
 })
+router.post('/attendances',async function(req,res){
+    try{
+        var attendances= await GetAttendances(req.body.room_name);
+        console.log(attendances);
+    }
+    catch(err){
+        res.status(500).json(err);
+        throw err;
+    }
+})
 let GetRoomList = async function(){
     var roomlist = await db.room.findAll({
         raw: true,
         attributes:['name'],
     });
     return roomlist;
+}
+let GetAttendances = async function(room_name){
+    var room = await db.room.findOne({
+        where:{
+            name:room_name
+        },
+        attributes:['ID']
+    });
+    var attendances = await db.user.findAll({
+        raw:true,
+        attributes:['Name'],
+        where:{
+            roomID:room.ID
+        }
+    });
+    return attendances;
 }
 module.exports=router;
