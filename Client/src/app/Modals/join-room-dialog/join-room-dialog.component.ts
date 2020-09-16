984fd94e0dc558bc2248d1a848819e0d52fbce12
Nguyen Ngoc Huy <nh01272639408@gmail.com>
2020-09-16 11:14:43 +0700

    fix enter key

--- Client/src/app/Modals/join-room-dialog/join-room-dialog.component.ts
+++ Client/src/app/Modals/join-room-dialog/join-room-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit ,Inject} from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { FormGroup, FormControlName, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { HostListener } from '@angular/core';
 @Component({
   selector: 'app-join-room-dialog',
   templateUrl: './join-room-dialog.component.html',
@@ -10,6 +11,13 @@ export class JoinRoomDialogComponent implements OnInit {
   private JoinForm: FormGroup;
   private room:FormControl;
   private password:FormControl;
+  
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) { 
+    if(event.key == "Enter"){
+      this.Submit();
+    }
+  }
   
   hide = true;
   constructor(private fb:FormBuilder,public dialogRef: MatDialogRef<JoinRoomDialogComponent>,@Inject(MAT_DIALOG_DATA) public data: any) {
