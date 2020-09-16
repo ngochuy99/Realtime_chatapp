5777d9d10aeba8604404d299f209c65e1da50689
Nguyen Ngoc Huy <nh01272639408@gmail.com>
2020-09-16 11:16:36 +0700

    Add logout button

--- Client/src/app/Component/toolbar/toolbar.component.ts
+++ Client/src/app/Component/toolbar/toolbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router } from '@angular/router'
+import { CookieService } from 'ngx-cookie-service';
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
@@ -9,11 +10,15 @@ import { Router } from '@angular/router';
 export class ToolbarComponent implements OnInit {
 
 
-  constructor(private router:Router) { }
+  constructor(private router:Router,private cookie:CookieService) { }
 
   ngOnInit() {
   }
   returnHomepage(){
     this.router.navigate(['homepage']);
+  }
+  signout(){
+    this.cookie.deleteAll();
+    this.router.navigate(['user/login']);
   }
 }
